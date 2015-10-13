79e380386c6f1b0a7b5c10f46dac9346bd8c2cdd
Sondre Husevold <sondrehusev@gmail.com>
2015-10-13 02:21:00 +0200

    Startet på OrderController og OrderView...

---
+++ Kaffeplaneten/Kaffeplaneten/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kaffeplaneten.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Ordre
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderView()
+        {
+            return View();
+        }
+    }
+}
