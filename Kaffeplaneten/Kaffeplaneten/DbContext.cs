561db84903a1ca81bdb6d3dc863afa696a207433
chrban <christer@bang.is>
2015-10-13 16:23:31 +0200

    The solution

---
+++ Kaffeplaneten/Kaffeplaneten/DbContext.cs
@@ -0,0 +1,31 @@
+namespace Kaffeplaneten
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DbContext : DbContext
+    {
+        // Your context has been configured to use a 'DbContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Kaffeplaneten.DbContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'DbContext' 
+        // connection string in the application configuration file.
+        public DbContext()
+            : base("name=DbContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Kaffeplaneten/Kaffeplaneten/Models/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kaffeplaneten.Models
+{
+    public class Customer
+    {
+
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Fornavn må oppgis")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Etternavn")]
+        [Required(ErrorMessage = "Etternavn må oppgis")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Epost")]
+        [Required(ErrorMessage = "Epost må oppgis")]
+        public string email { get; set; }
+
+        [Display(Name = "Telefon")]
+        [Required(ErrorMessage = "Telefon må oppgis")]
+        public string phone { get; set; }
+
+        public int customerID { get; set; }
+
+        [Display(Name = "Postnummer")]
+        [Required(ErrorMessage = "Postnummer må oppgis")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnummeret må være 4 siffer")]
+        public string zipCode { get; set; }
+
+        [Display(Name = "Poststed")]
+        [Required(ErrorMessage = "Poststed må oppgis")]
+        public string province { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Adresse må oppgis")]
+        public string adress { get; set; }
+
+        [Display(Name = "Betalingsadresse")]
+        [Required(ErrorMessage = "Betalingsadresse må oppgis")]
+        public string payAdress { get; set; }
+
+        [Display(Name = "Bet. Postnummer")]
+        [Required(ErrorMessage = "Betalingspostnummer må oppgis")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnummeret må være 4 siffer")]
+        public string payZipcode { get; set; }
+
+        [Display(Name = "Bet. Poststed")]
+        [Required(ErrorMessage = "Betalingspoststed må oppgis")]
+        public string payProvince { get; set; }
+
+
+    }
+}
