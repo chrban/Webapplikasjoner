28c5e771cb0cd3ea67781ed2f186a53cfb65e35f
chrban <christer@bang.is>
2015-10-14 20:53:52 +0200

    Navbar

---
+++ Kaffeplaneten/Kaffeplaneten/Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kaffeplaneten.Models
+{
+    public class ProductModel
+    {
+
+        public int productID { get; set; }
+        public string imageURL { get; set; }
+
+        [Display(Name = "Produktnavn:")]
+        public string productName { get; set; }
+        [Display(Name = "Varelager:")]
+        public int stock { get; set; }
+
+        [Display(Name = "Pris:")]
+        public double price { get; set; }
+
+        [Display(Name = "Kategori")]
+        public string category { get; set; }
+
+        [Display(Name = "Beskrivelse:")]
+        public string description { get; set; }
+    }
+}
