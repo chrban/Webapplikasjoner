4069abc3a4f79b8dddd0f5e3b96d8e5fda4549fc
chrban <christer@bang.is>
2015-10-15 02:19:24 +0200

    Litt producthandling

--- Kaffeplaneten/Kaffeplaneten/DBProduct.cs
+++ Kaffeplaneten/Kaffeplaneten/DBProduct.cs
@@ -11,7 +11,7 @@ namespace Kaffeplaneten
     {
 
 
-        public List<Products> MakeList()
+       /* public List<Products> MakeList()
         {
             var db = new CustomerContext();
             List<Products> listOfAllProducts = db.Products.ToList();
@@ -19,17 +19,53 @@ namespace Kaffeplaneten
             return listOfAllProducts;
 
         }
+        */
+
+
+        public List<ProductModel> MakeList()
+        {
+            var db = new CustomerContext();
+            List<Products> liste = db.Products.ToList();
+            List<ProductModel> utListe = new List<ProductModel>();
+
+            foreach(var i in liste)
+            {
+
+                utListe.Add(toObject(i));
+            }
+            return utListe;
+        }
 
         public Products getProduct(int pID)
         {
             var db = new CustomerContext();
-
             var product = db.Products.FirstOrDefault(p => p.productID == pID);
 
+            if(product == null)
+            {
+                //fant ikke produkt
+            }
             return product;
 
         }
 
+       
+        public ProductModel toObject(Products inProd)
+        {
+            var db = new CustomerContext();
+            var i = db.Products.Find(inProd.productID);
+
+            var newProduct = new ProductModel();
+            newProduct.productID = i.productID;
+            newProduct.productName = i.productName;
+            newProduct.price = i.price;
+            newProduct.imageURL = i.imageURL;
+            newProduct.description = i.description;
+            newProduct.category = i.category;
+            newProduct.stock = i.stock;
+            return newProduct;
+
+        }
 
 
     }
