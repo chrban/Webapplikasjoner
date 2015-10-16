c8a60eb0e0fb3f12797e2c7f999f9b5291a0b0b4
chrban <christer@bang.is>
2015-10-16 19:22:19 +0200

    Stor endring av alt som angår Products

--- Kaffeplaneten/Kaffeplaneten/DBProduct.cs
+++ Kaffeplaneten/Kaffeplaneten/DBProduct.cs
@@ -10,71 +10,25 @@ namespace Kaffeplaneten
     public class DBProduct
     {
 
-
-       /* public List<Products> MakeList()
+        //TODO - Christer: Skal endres (return hele ProductsDBlist)
+        public List<Products> getProductsByCategory(string kategori)
         {
             var db = new CustomerContext();
-            List<Products> listOfAllProducts = db.Products.ToList();
 
-            return listOfAllProducts;
+            var produkter = db.Products.Where(s => s.category == kategori).ToList();
 
+            Debug.WriteLine("LIST metode " + produkter);
+            return produkter;
         }
-        */
 
-
-        public List<ProductModel> MakeList()
+        //Brukes ikke
+        public List<Products> henteProdukter()
         {
             var db = new CustomerContext();
-            List<Products> liste = db.Products.ToList();
-            List<ProductModel> utListe = new List<ProductModel>();
 
-            foreach(var i in liste)
-            {
+            var produkter = db.Products.Where(s => s.productID == 1);
 
-                utListe.Add(toObject(i));
-            }
-            return utListe;
-        }
-
-        public Products getProduct(int pID)
-        {
-            var db = new CustomerContext();
-            var product = db.Products.FirstOrDefault(p => p.productID == pID);
-
-            if(product == null)
-            {
-                //fant ikke produkt
-            }
-            return product;
-
-        }
-
-       
-        public ProductModel toObject(Products inProd)
-        {
-            var db = new CustomerContext();
-            var i = db.Products.Find(inProd.productID);
-
-            var newProduct = new ProductModel();
-            newProduct.productID = i.productID;
-            newProduct.productName = i.productName;
-            newProduct.price = i.price;
-            newProduct.imageURL = i.imageURL;
-            newProduct.description = i.description;
-            newProduct.category = i.category;
-            newProduct.stock = i.stock;
-            return newProduct;
-
-        }
-
-
-    }
-
-
-
-
-
-
-    
-   
+            return produkter.ToList();
+         }
+    }   
 }
