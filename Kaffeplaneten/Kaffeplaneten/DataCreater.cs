75bfbfc2882fae28468911d0a1e32ba116a096ae
Sondre Husevold <sondrehusev@gmail.com>
2015-10-18 22:47:49 +0200

    Fikset ImgURL i DataCreator. Tullet med look på startsiden

--- Kaffeplaneten/Kaffeplaneten/DataCreater.cs
+++ Kaffeplaneten/Kaffeplaneten/DataCreater.cs
@@ -69,9 +69,17 @@ namespace Kaffeplaneten
 
         }
 
+        public static string getRandomImage()
+        {
+            string img = "img";
+            string jpg = ".jpg";
+            Random rnd = new Random();
+            return (img + rnd.Next(0, 5) + jpg);
+        }
+
         public static ProductModel createBrazillianBlend()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Brazillian Blend";
             p.description = "Vår rimelige hverdagskaffe. Lett fin frokostkaffe nesten helt uten garvesyre. Har du dårlig \"kaffemage\" er dette definitivt din kaffe.";
             p.price = 45;
@@ -81,7 +89,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createEtiopiskMokka()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Etiopisk Mokka";
             p.description = "Lyst brent kaffe med snev av vill ettersmak. Fin frokostkaffe. Meget populær.";
             p.price = 52;
@@ -91,7 +99,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createIndianMysore()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Indian Mysore";
             p.description = "Flott kaffe fra det sydlige India. God syrlighet og stor aroma. Lang ettersmak. En super kaffe.";
             p.price = 59;
@@ -101,7 +109,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createAstorLibano()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Astor Libano";
             p.description = "Dette er en kaffe du må unne deg. Meduim fyldig, frisk syrlighet og stor dybde og aroma. En fantastisk flott kaffe.";
             p.price = 55;
@@ -111,7 +119,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createMayfairBlend()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Mayfair Blend";
             p.description = "Medium fyldig kaffe. Allround kaffen som passer til det meste. Meget god blanding.";
             p.price = 49;
@@ -121,7 +129,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createMexicanCoffee()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Mexican Coffee";
             p.description = "Antydning til røyksmak. Fin sortering av ekstra store bønner. Medium til fyldig.";
             p.price = 59;
@@ -131,7 +139,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createExecutiveBlend()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Executive Blend";
             p.description = "Moka d'Or og High mountain blend blandet. Medium mørk med en fin avrundet smak.";
             p.price = 49;
@@ -141,9 +149,9 @@ namespace Kaffeplaneten
         }
         public static ProductModel createAfterDinnerBlend()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "After Dinner Blend";
-            p.description = "*Mørkbrent kaffe med 25% robusta. Dette gir en typisk \"europeisk\" smak kraftig og med antydning til bitter ettersmak. Ikke bare en ettermiddagskaffe, ypperlig også til å våkne på - gjerne med melk og brunt sukker.";
+            p.description = "Mørkbrent kaffe med 25% robusta. Dette gir en typisk \"europeisk\" smak kraftig og med antydning til bitter ettersmak. Ikke bare en ettermiddagskaffe, ypperlig også til å våkne på - gjerne med melk og brunt sukker.";
             p.price = 48;
             p.category = "Mørk";
             p.stock = 50;
@@ -151,7 +159,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createCafedeParis()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Cafe de Paris";
             p.description = "Meget mørk og kraftig kaffe. Bare for de aller tøffeste. Populær. Ypperlig også til Caffe Latte og Irish Coffee.";
             p.price = 48;
@@ -161,7 +169,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createCostaRicanTarrazu()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Costa Rican Tarrazu";
             p.description = "Mørk kaffe av aller fineste kvalitet. Passer i selskap til kaker og/eller cognac. Hva med en sigar også?";
             p.price = 59;
@@ -171,7 +179,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createPrimeHonduras()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Prime Honduras";
             p.description = "En singel plantasjekaffe av høy kvalitet. Mørk og fyldig. Vår bestselger!";
             p.price = 52;
@@ -181,7 +189,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createOldBrownJava()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Old Brown Java";
             p.description = "Dette er en kaffe mange har ventet på å få tilbake etter flere år i eksil. Den er kraftig med lang ettersmak og rik på undertoner av lær og tobakk. En utsøkt kaffe for de som vil ha det kraftfullt.";
             p.price = 59;
@@ -191,7 +199,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createKoffeinfriEspresso()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Koffeinfri Espresso";
             p.description = "Et godt alternativ til de som ikke kan ha koffein eller som rett og slett bare vil bevare sin gode nattesøvn.";
             p.price = 59;
@@ -201,7 +209,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createKoffeinfriKaffe()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Koffeinfri Kaffe";
             p.description = "Hvis ikke du vet at dette er koffeinfri kaffe, ville du aldri tro det. Smaker helt som kaffe med koffein. Dette er dessuten kaffe av høyeste kvalitet som utgangspunkt. Vi våger påstanden: Du har aldri smakt bedre koffeinfri.";
             p.price = 59;
@@ -211,7 +219,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createChocoVanilje()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Choco Vanilje";
             p.description = "Aromafisert";
             p.price = 69;
@@ -221,7 +229,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel VaniljeHasselnøtt()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Vanilje Hasselnøtt";
             p.description = "Populær blanding av nøtt og vaniljesmak.";
             p.price = 69;
@@ -231,7 +239,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createNutCream()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Nut Cream";
             p.description = "Kaffe tilsatt aroma med nøtt og fløtesmak. Meget god og populær kaffe.";
             p.price = 65;
@@ -241,7 +249,7 @@ namespace Kaffeplaneten
         }
         public static ProductModel createIrishCream()
         {
-            var p = new ProductModel();
+            var p = new ProductModel(); p.imageURL = getRandomImage();
             p.productName = "Irish Cream";
             p.description = "Kaffe tilsatt aroma med Baileys smak. Den desiderte bestselger innen smakstilsatt kaffe.";
             p.price = 69;
