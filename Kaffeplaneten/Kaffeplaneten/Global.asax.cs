05eccb4ebb74fdd7750834269caa8ee7dd06f8f1
Magnus Tønsager <mt737@hotmail.com>
2015-10-18 21:43:32 +0200

    Lagt til metode for å legge produkter inn i databasen

--- Kaffeplaneten/Kaffeplaneten/Global.asax.cs
+++ Kaffeplaneten/Kaffeplaneten/Global.asax.cs
@@ -13,10 +13,10 @@ namespace Kaffeplaneten
     {
         void Application_Start(object sender, EventArgs e)
         {
-           //Code that runs on application startup
-           //TestingClass.main(new string[0]);
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //Code that runs on application startup
+            //DataCreater.addProducts();//Slå på denne for å legge produkter inn i databasen. Husk å slå av igjen!
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
