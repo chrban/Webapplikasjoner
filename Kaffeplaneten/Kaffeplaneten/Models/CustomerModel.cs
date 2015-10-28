9b6335a042033a75366c6e5aac771fcbc95b5326
Magnus Johan Knalstad <miniknall92@hotmail.com>
2015-10-28 09:28:43 +0100

    FIkset opp Registreringen optimal for Kunde som designmessig også kan brukes for Admin i orden

--- Kaffeplaneten/Kaffeplaneten/Models/CustomerModel.cs
+++ Kaffeplaneten/Kaffeplaneten/Models/CustomerModel.cs
@@ -10,17 +10,17 @@ namespace Kaffeplaneten.Models
     {
 
 
-        [Display(Name = "Skriv fornavn")]
+        [Display(Name = "Fornavn: ")]
         [Required(ErrorMessage = "Fornavn må oppgis")]
         [RegularExpression(@"[A-ZÆØÅ][a-zæøå ,.'-]{2,50}", ErrorMessage = "Du må ha stor forbokstav og fornavnet må være lengre enn 3 bokstaver. Max-lengde er 50 ")]
         public string firstName { get; set; }
 
-        [Display(Name = "Skriv Etternavn:")]
+        [Display(Name = "Etternavn: ")]
         [Required(ErrorMessage = "Etternavn må oppgis")]
         [RegularExpression(@"[A-ZÆØÅ][a-zæøå]{1,50}", ErrorMessage = "Du må ha stor forbokstav og etternavnet må være lengre enn 2 bokstaver. Max-lengde er 50 ")]
         public string lastName { get; set; }
 
-        [Display(Name = "Skriv Brukernavn(Epost)")]
+        [Display(Name = "Brukernavn(Epost): ")]
         [Required(ErrorMessage = "Epost må oppgis")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Feil ved opprettelse av epost. Eksempel på struktur: 'Ola@nordmann.no'")]
         public string email { get; set; }
@@ -28,7 +28,7 @@ namespace Kaffeplaneten.Models
         [Required(ErrorMessage = "Passord må oppgis")]
         [StringLength(50, MinimumLength = 8, ErrorMessage = "Passordet må inneholde minimum 8 tegn")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")]
-        [Display(Name = "Skriv inn et passord")]
+        [Display(Name = "Passord:")]
         public string password { get; set; }
         public bool passwordStrength { get; set; }
 
@@ -38,39 +38,39 @@ namespace Kaffeplaneten.Models
         public string passwordVerifier { get; set; }
 
 
-        [Display(Name = "Skriv Telefon")]
+        [Display(Name = "Telefon: ")]
         [Required(ErrorMessage = "Telefon må oppgis")]
         [RegularExpression(@"[0-9]{8}", ErrorMessage = "Telefonnummeret må være 8 siffer langt")]
         public string phone { get; set; }
 
         public int customerID { get; set; }
 
-        [Display(Name = "Skriv postnummer")]
+        [Display(Name = "Postnummer: ")]
         [Required(ErrorMessage = "Postnummer må oppgis")]
         [RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnummeret må være 4 siffer langt")]
         public string zipCode { get; set; }
 
-        [Display(Name = "Skriv poststed:")]
+        [Display(Name = "Poststed:")]
         [Required(ErrorMessage = "Poststed må oppgis")]
         public string province { get; set; }
 
-        [Display(Name = "Skriv adresse:")]
+        [Display(Name = "Adresse:")]
         [Required(ErrorMessage = "Adresse må oppgis")]
         public string adress { get; set; }
 
         [Display(Name = "Samme levering og betalingsadresse?")]
         public bool sameAdresses { get; set; }
 
-        [Display(Name = "Skriv Betalingsadresse")]
+        [Display(Name = "Betalingsadresse: ")]
         [Required(ErrorMessage = "Betalingsadresse må oppgis")]
         public string payAdress { get; set; }
 
-        [Display(Name = "Skriv Bet. Postnummer")]
+        [Display(Name = "Bet. Postnummer: ")]
         [Required(ErrorMessage = "Betalingspostnummer må oppgis")]
         [RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnummeret må være 4 siffer")]
         public string payZipcode { get; set; }
 
-        [Display(Name = "Skriv Bet. Poststed")]
+        [Display(Name = "Bet. Poststed: ")]
         [Required(ErrorMessage = "Betalingspoststed må oppgis")]
         public string payProvince { get; set; }
     }
