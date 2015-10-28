584a2de38360439a940c9b16234e5c6992db824a
Sondre Husevold <sondrehusev@gmail.com>
2015-10-28 14:07:01 +0100

    Laget en LoggingBLL og en LoggingDAL.
    LoggingDAL ettersom den skriver ting på serveren og ikke lokalt. Ifølge: http://www.databaseskill.com/261891/ har den ansvar til å skrive eller lese tekst, binærfiler eller XML/Json  dokumenter på serveren.
    
    Disse er ikke testet, og må testes før de blir tatt i bruk.

---
+++ Kaffeplaneten/BLL/LoggingBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kaffeplaneten.DAL;
+using Kaffeplaneten.Models;
+
+namespace Kaffeplaneten.BLL
+{
+    class LoggingBLL
+    {
+        private LoggingDAL _loggingDAL;
+
+        public LoggingBLL()
+        {
+            _loggingDAL = new LoggingDAL();
+        }
+
+        public bool logToUser(Persons user, string action) {
+            return _loggingDAL.logToUser(user, action);
+        }
+        public bool logToDatabase(string action)
+        {
+            return _loggingDAL.logToDatabase(action);
+        }
+
+        // Find messages with certain criteria.
+        // Needs to be done.
+        public bool findInDatabaseLog(string criteria)
+        {
+            return _loggingDAL.findInDatabaseLog(criteria);
+        }
+        public bool findInInteractionLog(string criteria)
+        {
+            return _loggingDAL.findInInteractionLog(criteria);
+        }
+
+        public bool createLog(string type)
+        {
+            return _loggingDAL.createLog(type);
+        }
+
+
+    }
+}
---
+++ Kaffeplaneten/DAL/LoggingDAL.cs
@@ -0,0 +1,105 @@
+﻿using Kaffeplaneten.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeplaneten.DAL
+{
+    public class LoggingDAL
+    {
+        public const string LOG_DATABASE = "../../Content/log_database.txt";
+        public const string LOG_INTERACTION = "../../Content/log_interaction.txt";
+
+        public bool logToUser(Persons user, string message)
+        {
+            createLog(LOG_INTERACTION);
+            string logLine = "{ " +
+                              "'User': '" + user.firstName + " " + user.lastName + "'," +
+                              "'Date': '" + DateTime.Now.ToString("h:mm:ss tt") + "'," +
+                              "'Action': '" + message + "'," +
+                          " }";
+            try
+            {
+                using (TextWriter logWriter = new StreamWriter(LOG_INTERACTION))
+                {
+                    logWriter.WriteLine(logLine);
+                    logWriter.Close();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("ERROR: COULD NOT LOG ACTION TO USER.");
+                return false;
+            }
+        }
+        public bool logToDatabase(string message)
+        {
+            createLog(LOG_DATABASE);                            // Checks for log existence.
+            string logLine = "{ " +
+                              "'Date': '" + DateTime.Now.ToString("h:mm:ss tt") + "'," +
+                              "'Action': '" + message + "'," +
+                          " }";
+            try
+            {
+                using (TextWriter logWriter = new StreamWriter(LOG_DATABASE))
+                {
+                    logWriter.WriteLine(logLine);
+                    logWriter.Close();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("ERROR: COULD NOT LOG DATABASE ACTION.");
+                return false;
+            }
+        }
+
+        // Find messages with certain criteria.
+        // Needs to be done.
+        public bool findInDatabaseLog(string criteria)
+        {
+            return false;
+        }
+        public bool findInInteractionLog(string criteria)
+        {
+            return false;
+        }
+
+        public bool createLog(string type)
+        {
+            if (type.Equals(LOG_DATABASE))
+            {
+                if (!System.IO.File.Exists(LOG_DATABASE))
+                {
+                    System.IO.File.Create(LOG_DATABASE).Dispose();
+                    using (TextWriter logWriter = new StreamWriter(LOG_DATABASE))
+                    {
+                        logWriter.WriteLine("Log file created: " + DateTime.Now.ToString("h:mm:ss tt"));
+                        logWriter.Close();
+                        return true;
+                    }
+                }
+            }
+            else if (type.Equals(LOG_INTERACTION))
+            {
+                if (!System.IO.File.Exists(LOG_INTERACTION))
+                {
+                    System.IO.File.Create(LOG_INTERACTION).Dispose();
+                    using (TextWriter logWriter = new StreamWriter(LOG_INTERACTION))
+                    {
+                        logWriter.WriteLine("Log file created: " + DateTime.Now.ToString("h:mm:ss tt"));
+                        logWriter.Close();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
