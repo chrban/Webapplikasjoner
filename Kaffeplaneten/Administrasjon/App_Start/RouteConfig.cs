b6b7f3f6aca07e9a8551da6873f856cec8f29607
Magnus Johan Knalstad <miniknall92@hotmail.com>
2015-10-29 20:04:49 +0100

    Fikset multiple startups slik at både Kaffeplaneten og Administrasjon debuges og starter samtidig ved kjøring

--- Kaffeplaneten/Administrasjon/App_Start/RouteConfig.cs
+++ Kaffeplaneten/Administrasjon/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Administrasjon
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Super", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
