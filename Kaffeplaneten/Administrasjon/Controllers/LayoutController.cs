749df9506a14cd0235b9456383637de2fb946f0f
Magnus Johan Knalstad <miniknall92@hotmail.com>
2015-10-29 23:08:27 +0100

    Untracked files inkludert commit

---
+++ Kaffeplaneten/Administrasjon/Controllers/LayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Administrasjon.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: Layout
+        public ActionResult HeaderAndMenuBar()
+        {
+            return View();
+        }
+    }
+}
