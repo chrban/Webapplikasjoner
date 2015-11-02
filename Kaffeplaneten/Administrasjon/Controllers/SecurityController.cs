f6ce658356550dbbe232a292f9c7f58593026cf0
Magnus Johan Knalstad <miniknall92@hotmail.com>
2015-11-02 17:19:44 +0100

    Commit forskjellig

---
+++ Kaffeplaneten/Administrasjon/Controllers/SecurityController.cs
@@ -0,0 +1,71 @@
+﻿using Kaffeplaneten.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using Kaffeplaneten.BLL;
+using Administrasjon.Controllers;
+
+namespace Kaffeplaneten.Controllers
+{
+    public class SecurityController : SuperController
+    {
+        private UserBLL _userBLL;
+        private CustomerBLL _customerBLL;
+
+        public SecurityController()
+        {
+            _userBLL = new UserBLL();
+            _customerBLL = new CustomerBLL();
+        }
+        public ActionResult Loginview()
+        {
+            if (Session[LOGGED_INN] == null)
+            {
+                Session[LOGGED_INN] = false;
+                ViewBag.LoggedOn = false;
+            }
+            else
+            {
+                ViewBag.LoggedOn = Session[LOGGED_INN];
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Loginview(UserModel user)
+        {
+            user.passwordHash = base.getHash(user.password);
+            if (_userBLL.verifyUser(user))
+            {
+                Session[LOGGED_INN] = true;
+                ViewBag.LoggedOn = true;
+                Session[CUSTOMER] = _customerBLL.find(user.username);
+                return RedirectToAction("AllProducts", "Product", user.username);
+            }
+            ModelState.AddModelError("", "Feil brukernavn eller passord");
+            return View();
+        }
+        public ActionResult LoggedIn()
+        {
+            if (Session[LOGGED_INN] != null)
+            {
+                bool loggetInn = (bool)Session[LOGGED_INN];
+                if (loggetInn)
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult LoggedOut()
+        {
+            Session[LOGGED_INN] = null;
+            Session[CUSTOMER] = null;
+            return RedirectToAction("Loginview");
+        }
+    }
+}
