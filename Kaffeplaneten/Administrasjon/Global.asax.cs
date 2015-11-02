eace49a55b28e2ace41b6b10317d1e96b1909c87
Magnus Tønsager <mt737@hotmail.com>
2015-11-02 15:25:11 +0100

    Fjernet adresser fra employee

--- Kaffeplaneten/Administrasjon/Global.asax.cs
+++ Kaffeplaneten/Administrasjon/Global.asax.cs
@@ -11,6 +11,7 @@ namespace Administrasjon
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
