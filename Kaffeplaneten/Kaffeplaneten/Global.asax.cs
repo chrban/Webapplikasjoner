6e0a099cc1e72ca851103f91f224946e85e09eff
Sondre Husevold <sondrehusev@gmail.com>
2015-11-07 22:18:20 +0100

    Mer commiting.

--- Kaffeplaneten/Kaffeplaneten/Global.asax.cs
+++ Kaffeplaneten/Kaffeplaneten/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
+using System.IO;
 using System.Web.SessionState;
+using System.Diagnostics;
 
 namespace Kaffeplaneten
 {
